c054c3c8423230dd045043e80aaae9648b02202e
DESKTOP-SFRJN2I\Newton <yankaiyin@hotmail.com>
2023-07-05 15:11:40 -0400

    install shadcn and custom font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
+<script lang="ts">
+	import {
+		Card,
+		CardContent,
+		CardDescription,
+		CardFooter,
+		CardHeader,
+		CardTitle
+	} from '$components/ui/card';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Card>
+	<CardHeader>
+		<CardTitle>Card Title</CardTitle>
+		<CardDescription>Card Description</CardDescription>
+	</CardHeader>
+	<CardContent>
+		<p>Card Content</p>
+	</CardContent>
+	<CardFooter>
+		<p>Card Footer</p>
+	</CardFooter>
+</Card>
--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,28 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
-
-/** @type {import('mdsvex').MdsvexOptions} */
+/** @type {import('mdsvex').MdsvexOptions}*/
 const mdsvexOptions = {
 	extensions: ['.md']
 };
-
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: 'src/lib/components',
+			'$components/*': 'src/lib/components/*'
+		}
 	},
-	extensions: ['.svelte', '.md']
+	extensions: ['.svelte', '.md'],
+	shadcn: {
+		componentPath: './src/lib/components/ui'
+	}
 };
-
 export default config;
